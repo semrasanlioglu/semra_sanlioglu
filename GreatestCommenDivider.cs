7211738477fb6c0c17ad8d8d71fdcc3ec987f402
semrasanlioglu <59733848+semrasanlioglu@users.noreply.github.com>
2020-01-14 23:13:04 +0300

    Add files via upload

---
+++ GreatestCommenDivider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCD
+{
+    class GreatestCommenDivider
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two numbers: ");
+            int num1, num2;
+            num1 = int.Parse(Console.ReadLine());
+            num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("GCD : "+ Greatestcommondivisor(num1, num2));
+        }
+
+        public static int Greatestcommondivisor(int a, int b)
+        {
+            if (b != 0)
+                return Greatestcommondivisor(b, a % b);
+            return a;
+        }
+    }
+}
---
+++ ReversedNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReversingDigits
+{
+    class ReversedNumber
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number: ");
+            int number;
+            number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Reverse(number));
+
+        }
+
+        public static int Reverse(int n)
+        {
+            int r, reversedNumber=0;
+            while (n != 0)
+            {
+                r = n % 10;
+                reversedNumber = reversedNumber * 10 + r;
+                n /= 10;
+            }
+
+            return reversedNumber;
+        }
+    }
+}
